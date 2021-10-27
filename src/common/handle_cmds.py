5bf5c5e20ac74f3474f98b9de0f45fc0cee75a3b
papajohn <papajohn@upatras.gr>
2021-10-27 17:05:25 +0300

    Execute command in shell when action arrives

--- src/common/handle_cmds.py
+++ src/common/handle_cmds.py
@@ -1,9 +1,34 @@
 import json
+import subprocess
 
 class CmdHandler:
     def __init__(self):
         self.action=None
         self.action_command=None
+
+
+    def _execCMD(self):
+        #process = subprocess.Popen(['echo', self.action_command], 
+        print("--->0")
+        process = subprocess.Popen([self.action_command],
+                           #we need shell= true to pass the command as string and not as list
+                           shell=True, 
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True)
+        print("--->1")
+        while True:
+            print("--->2")
+            output = process.stdout.readline()
+            print(output.strip())
+            # Do something else
+            return_code = process.poll()
+            if return_code is not None:
+                print('RETURN CODE', return_code)
+                # Process has finished, read rest of the output 
+                for output in process.stdout.readlines():
+                    print(output.strip())
+                break
+
 
 
     def executeCMD(self):
@@ -12,5 +37,6 @@ class CmdHandler:
         print(self.action)
         print(self.action_command)
         print("***********")
+        self._execCMD()
 
 
