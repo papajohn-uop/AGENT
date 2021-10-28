31bc4339dfcac32c70e7ac29e4e093fb5025d232
papajohn <papajohn@upatras.gr>
2021-10-28 11:18:52 +0300

    Minor change in title

--- src/openapi_server/main.py
+++ src/openapi_server/main.py
@@ -20,7 +20,7 @@ from  common import handle_files,handle_cmds
 
 
 app = FastAPI(
-    title="API Resource Inventory Management",
+    title="AGENT API Resource Inventory Management",
     description="## TMF API Reference: TMF639 - Resource Inventory   ### Release : 19.5 - December 2019  Resource Inventory  API goal is to provide the ability to manage Resources.  ### Operations Resource Inventory API performs the following operations on the resources : - Retrieve an entity or a collection of entities depending on filter criteria - Partial update of an entity (including updating rules) - Create an entity (including default values and creation rules) - Delete an entity (for administration purposes) - Manage notification of events",
     version="4.0.0",
 )
