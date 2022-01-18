160d6dce1c8001c66934382eb1a77ff286d4dbff
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-01-18 15:37:08 +0200

    Changes in the creation of resource durin start up

--- server/src/common/handle_files.py
+++ server/src/common/handle_files.py
@@ -19,10 +19,11 @@ class FileHandler:
         self.allowed_actions=list()
         self.commands=dict()
         self.allowed_params=None
-        self.resource=None
+        self.resource_data=None
         self.server=None
         self.resourceID=None
         self.registered=None
+        self.resource=None #this will be the resource obj
           
     def read_conf(self):
         with open(self.agent_conf_file, "r") as jsonfile:
@@ -34,8 +35,8 @@ class FileHandler:
                 for key in data["commands"]:
                     self.allowed_actions.append(key)
             self.allowed_params=data["allowed_params"]
-            if "resource" in data:
-                self.resource=data["resource"]
+            if "resource_data" in data:
+                self.resource_data=data["resource_data"]
             if "resourceID" in data:
                 self.resourceID=data["resourceID"]
             if "Registered" in data:
@@ -63,34 +64,43 @@ class FileHandler:
                 else:
                     print("Yeah  this param is not available... ",param)
 
+
+    #create a resource in order to register
+    def __createResource(self):
+        #TODO: Check if entries exist in cfg
+        #create a random name so that DB does not get an error when testing
+        ran = ''.join(random.choices(string.ascii_uppercase + string.digits +string.ascii_lowercase, k = 5))    
+        print(self.resource_data)
+        selfResource=ResourceCreate(name=self.resource_data["name"]+ran)
+        #Send fixed name. This must be unique
+        selfResource=ResourceCreate(name=self.resource_data["name"])
+        selfResource.category=self.resource_data["category"]
+        selfResource.description=self.resource_data["description"]
+        selfResource.resource_version="0.0.1"
+        selfResource.resource_characteristic=[]
+        if "ip" in self.resource_data:
+            resourceIP_Char=Characteristic(name="IP",value={"value":self.resource_data["ip"]})
+            resourceIP_Char.id="string"
+            resourceIP_Char.value_type="string"
+            selfResource.resource_characteristic.append(resourceIP_Char)
+        if "location" in self.resource_data:
+            resourceLoc_Char=Characteristic(name="location",type="array",value={"value":self.resource_data["location"]})
+            resourceLoc_Char.id="string"
+            resourceLoc_Char.value_type="array"
+        return selfResource   
+
     #This will create the request to self register
     def selfRegister(self):
         print("Trying to self register")
         #check if registered already
         if  self.registered:
             print("Seems we are registered already")
+            #TODO: At this point we should send a GET to the server with our ID to create the selfResourse
+            #If the GET fails (i.e. the server has deleted us) we should register again
+            print(self.resource)
             return
-        #TODO: Check if entries exist in cfg
-        #create a random name so that DB does not get an error when testing
-        ran = ''.join(random.choices(string.ascii_uppercase + string.digits +string.ascii_lowercase, k = 5))    
-        print(self.resource)
-        selfResource=ResourceCreate(name=self.resource["name"]+ran)
-        #Send fixed name. This must be unique
-        selfResource=ResourceCreate(name=self.resource["name"])
-        selfResource.category=self.resource["category"]
-        selfResource.description=self.resource["description"]
-        selfResource.resource_version="0.0.1"
-        selfResource.resource_characteristic=[]
-        if "ip" in self.resource:
-            resourceIP_Char=Characteristic(name="IP",value={"value":self.resource["ip"]})
-            resourceIP_Char.id="string"
-            resourceIP_Char.value_type="string"
-            selfResource.resource_characteristic.append(resourceIP_Char)
-        if "location" in self.resource:
-            resourceLoc_Char=Characteristic(name="location",type="array",value={"value":self.resource["location"]})
-            resourceLoc_Char.id="string"
-            resourceLoc_Char.value_type="array"
-            selfResource.resource_characteristic.append(resourceLoc_Char)    
+
+        selfResource=self.__createResource() 
         print(selfResource)
 
         if self.allowed_actions is not None:
@@ -130,6 +140,10 @@ class FileHandler:
                     os.remove(self.agent_conf_file)
                     with open(self.agent_conf_file, 'w') as jsonfile:
                         json.dump(tmpData, jsonfile, indent=4)
+
+                    #all good
+                    self.resource=selfResource
+                    print(self.resource)
 
                 else:
                     print("This is strange")
