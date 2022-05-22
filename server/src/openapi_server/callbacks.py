1147abfb2c65ea600aabec2247ba23a05fc5a3f2
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-05-22 19:37:41 +0300

    Remove not required callbacks

--- server/src/openapi_server/callbacks.py
+++ server/src/openapi_server/callbacks.py
@@ -21,8 +21,6 @@ def process_event(event):
 
 
 def _execCMD(action_command):
-    #process = subprocess.Popen(['echo', self.action_command], 
-#    process = subprocess.Popen(["echo \"", action_command,"\" >>./test.txt"],
     print(type(action_command))
     print(action_command)
     process = subprocess.Popen(["echo \""+ str(action_command)+"\" >./pipe_cmd.txt"],
@@ -85,63 +83,3 @@ def echo():
     # _execCMD("echo 3 >>./test.txt ")    
 ########################
 
-TARGET_PATH="~/Amarisoft_LTE/enb/config/"
-TARGET_FILE="dummy_link_gnb.cfg "
-def changeConf(config):
-    cmd="echo 'Uplink Configuration' >>./test.txt ; "
-    cmd=cmd + "ln -sf " 
-    cmd=cmd + TARGET_PATH
-    cmd=cmd + config
-    cmd=cmd + TARGET_PATH 
-    cmd=cmd + TARGET_FILE
-    print("in change conf")
-    print(cmd)
-    return cmd
-
-def ul_config():
-    cmd=changeConf("LimeNet5G_N78_UL.cfg ")
-    _execCMD(cmd)
-    # _execCMD("echo 'Uplink Configuration' >>./test.txt ; ln -sf ~/Amarisoft_LTE/enb/config/LimeNet5G_N78_UL.cfg ~/Amarisoft_LTE/enb/config/dummy_link_gnb.cfg ")    
-
-def st_config():
-    cmd=changeConf("LimeNet5G_N78.cfg ")
-    _execCMD(cmd)
-    # _execCMD("echo 'Standard Configuration' >>./test.txt ; ln -sf ~/Amarisoft_LTE/enb/config/LimeNet5G_N78.cfg ~/Amarisoft_LTE/enb/config/dummy_link_gnb.cfg")    
-
-def ulmimo_config():
-    cmd=changeConf("LimeNet5G_N78_UL_MIMO.cfg ")
-    _execCMD(cmd)
-    # _execCMD("echo 'Uplink Mimo Configuration' >>./test.txt ;  ln -sf ~/Amarisoft_LTE/enb/config/LimeNet5G_N78_UL_MIMO.cfg ~/Amarisoft_LTE/enb/config/dummy_link_gnb.cfg ")    
-
-
-def twoslice_config():
-    cmd=changeConf("LimeNet5G_N78_2slices.cfg ")
-    _execCMD(cmd)
-    # _execCMD("echo 'Two slice Configuration' >>./test.txt ;  ln -sf ~/Amarisoft_LTE/enb/config/LimeNet5G_N78_2slices.cfg ~/Amarisoft_LTE/enb/config/dummy_link_gnb.cfg ")    
-
-
-def set_generic_config():
-    _execCMD("echo 'SET_GENERIC_CONFIG_FROM_CALLBACK' >> nikos.txt")
-
-
-def myCallBack_2():
-    print ("Callback2!")
-
-def myCallBack_3():
-    print ("Callback3!")
-
-
-def cmd2():
-    print ("\n\nCMD2 CB\n\n")
-
-
-#register_callback("event1",myCallBack_1)
-
-
-# call the onclick() method when the 'click' event happens on the button
-#some_api.register_callback(button, 'click', onclick)
-
-# print("Here...")
-# print(callbacks)
-# process_event("event1")
-# process_event("event2")
